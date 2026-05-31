e34eb2e2ec49b11a940f2681f096cfd50fb2f9df
Pace Thomson <pace.thomson@tcnbroadcasting.com>
2026-05-31 15:51:43 -0600

    Trying to fix app store deploy fail

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,4 +1,6 @@
 // Load .env so EXPO_PUBLIC_* vars are available (e.g. EXPO_PUBLIC_GOOGLE_ROUTES_API_KEY)
 require('dotenv').config();
 
-module.exports = require('./app.json');
+module.exports = ({ config }) => ({
+  ...config,
+});
